66e346639808ba16d629f36208bae52c3e5ef356
SE80 <se80@gmx.net>
2022-02-17 11:02:34 +0100

    9. Component Configuration

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # sap-ui5-walkthrough
 https://sapui5.hana.ondemand.com/#/entity/sap.m.tutorial.walkthrough
 
-Setup
-Step 1: Hello World!
-Step 2: Bootstrap
-Step 3: Controls
-Step 4: XML Views
-Step 5: Controllers
-Step 6: Modules
-Step 7: JSON Model
-Step 8: Translatable Texts 
+0. Setup
+1. Hello World!
+2. Bootstrap
+3. Controls
+4. XML Views
+5. Controllers
+6. Modules
+7. JSON Model
+8. Translatable Texts 
+9. Component Configuration
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,28 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict"
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onInit: function () {
-            var oData = {
-                recipient: {
-                    name: "UI5"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-
-            var i18nModel = new ResourceModel( {
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLokale: ""
-            });
-            this.getView().setModel(i18nModel, "i18n");
-        },
         onShowHello: function () {
 
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
